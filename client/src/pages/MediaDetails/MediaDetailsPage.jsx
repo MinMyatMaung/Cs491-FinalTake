3d76b5b345a06854a3bbc352fd1adc8e829545d9
Min Myat Maung <rickyminmyatmaung@gmail.com>
2026-05-14 00:50:33 -0700

    Update MediaDetailsPage.jsx

--- client/src/pages/MediaDetails/MediaDetailsPage.jsx
+++ client/src/pages/MediaDetails/MediaDetailsPage.jsx
@@ -264,7 +264,7 @@ const MediaDetailsPage = () => {
           </button>
           {user ? (
             <>
-              <span className="user-email">{user.email}</span>
+              <span className="user-email">{user.username}</span>
               <button className="btn btn-secondary" onClick={() => navigate('/profile')}>
                 Profile
               </button>
